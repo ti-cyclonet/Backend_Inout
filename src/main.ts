41040a53673b5230cbc971d7dafc7e104cc10a58
Alfredo Mamby Bossa <alfredo.mamby@aossas.com>
2026-03-30 18:58:39 -0500

    fix: add production domains to CORS

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,11 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ['http://localhost:4200', 'http://localhost:4201'],
+    origin: [
+      'http://localhost:4200', 'http://localhost:4201',
+      'https://auth.cyclonet.com.co', 'https://billing.cyclonet.com.co', 'https://app.cyclonet.com.co',
+      'https://master.d249aa02o69249.amplifyapp.com', 'https://master.dccjshhnh1byc.amplifyapp.com', 'https://master.d31x1n66is2877.amplifyapp.com',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'x-tenant-id'],
